ab95e3677504df01cafcd270fc5ef3c9ae0b65bf
Danny van Kooten <hi@dvk.co>
2026-03-23 10:05:12 +0100

    nit: remove unused code and update README to show arguments

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Koko Analytics Seeder
 
-This plugin registers a new WP CLI command to seed your WordPress database with sample data.
+This plugin registers a new WP CLI command to seed your WordPress database with sample data for the Koko Analytics plugin.
 
 ```
-wp koko-analytics-seed
+wp koko-analytics-seed --years=30 --paths=100 --referrers=100
 ```
 
-Running the command will generate 10000 posts, 1000 referrer URL's and 30 years worth of analytics data for [Koko Analytics](https://www.kokoanalytics.com/).
+Running this command will generate 100 paths, 100 referrer URL's and 30 years worth of analytics data for [Koko Analytics](https://www.kokoanalytics.com/).
 
 For the reposistory of Koko Analytics itself, please see [ibericode/koko-analytics](https://github.com/ibericode/koko-analytics).
 
--- src/Command.php
+++ src/Command.php
@@ -6,31 +6,6 @@ use WP_CLI;
 
 class Command
 {
-    // Ensures site has at least 100 posts
-    private function create_posts(int $n_posts): array
-    {
-        global $wpdb;
-
-        $count = (int) $wpdb->get_var("SELECT COUNT(*) FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish'");
-        $n =  max(0, ($n_posts - $count));
-
-        $progress = \WP_CLI\Utils\make_progress_bar( 'Generating posts', $n );
-        for ($i = 0; $i < $n; $i++) {
-            wp_insert_post([
-                'post_status' => 'publish',
-                'post_title' => "Post #{$i}",
-                'post_content' => "Hello world. This is the content of post #{$i}.",
-            ]);
-            $progress->tick();
-        }
-        $progress->finish();
-
-
-        return array_map(function ($row) {
-            return $row->ID;
-        }, $wpdb->get_results("SELECT ID FROM {$wpdb->posts} WHERE post_status = 'publish'"));
-    }
-
     public function create_referrer_urls(int $n_referrers): array
     {
         global $wpdb;
@@ -91,7 +66,6 @@ class Command
 
         $years = $options['years'] ?? 3;
         $n_paths = $options['paths'] ?? 100;
-        $n_posts = $options['posts'] ?? 100;
         $n_referrers = $options['referrers'] ?? 100;
 
         // empty current data
@@ -103,7 +77,6 @@ class Command
 
         $paths = $this->create_paths($n_paths);
         $referrers = $this->create_referrer_urls($n_referrers);
-        $referrer_count = count($referrers);
 
         $progress = \WP_CLI\Utils\make_progress_bar( 'Generating stats', $years * 365 );
         for ($dt = new \DateTimeImmutable('now'); $dt > new \DateTimeImmutable("-{$years} years"); $dt = $dt->modify('-1 day')) {
@@ -118,33 +91,44 @@ class Command
             }
 
             $wpdb->query($wpdb->prepare("INSERT INTO {$wpdb->prefix}koko_analytics_site_stats(date, pageviews, visitors) VALUES(%s, %d, %d)", array($date, $pageviews, $visitors)));
-            $values = array();
-            $pageviews_per_post = $pageviews / count($paths) * 2;
-            foreach ($paths as $path_id) {
-                $post_pageviews = (int) ($pageviews_per_post * rand(5, 15) * 0.1);
-                $post_visitors = (int) (rand(1, 3) * $post_pageviews * 0.1);
-                array_push($values, $date, $path_id, 0, $post_pageviews, $post_visitors);
-
-            }
-          
-            $placeholders = rtrim(str_repeat('(%s,%d,%d,%d,%d),', count($paths)), ',');
-            $wpdb->query($wpdb->prepare("INSERT INTO {$wpdb->prefix}koko_analytics_post_stats(date, path_id, post_id, pageviews, visitors) VALUES {$placeholders}", $values));
-
-            $values = array();
-            $pageviews_per_referrer = $pageviews / $referrer_count * 2;
-            foreach ($referrers as $referrer_id) {
-                $referrer_pageviews = (int) ($pageviews_per_referrer * rand(5, 15) * 0.1);
-                $referrer_visitors = (int) (rand(1, 3) * $referrer_pageviews * 0.1);
-                array_push($values, $date, $referrer_id, $referrer_pageviews, $referrer_visitors);
-            }
-            $placeholders = rtrim(str_repeat('(%s,%d,%d,%d),', count($referrers)), ',');
-            $wpdb->query($wpdb->prepare("INSERT INTO {$wpdb->prefix}koko_analytics_referrer_stats(date, id, pageviews, visitors) VALUES {$placeholders}", $values));
+            
+            $this->seed_post_stats($paths, $date, $pageviews);
+            $this->seed_referrers($referrers, $date, $pageviews);
 
             $progress->tick();
         }
 
         $progress->finish();
-        WP_CLI::success("Finished seeding Koko Analytics with {$years} years of data, {$n_paths} paths, {$n_posts} posts and {$n_referrers} referrers.");
+        WP_CLI::success("Finished seeding Koko Analytics with {$years} years of data, {$n_paths} paths and {$n_referrers} referrers.");
+    }
 
+    protected function seed_post_stats(array $paths, string $date, int $pageviews): void
+    {
+        global $wpdb;
+        $values = array();
+        $pageviews_per_post = $pageviews / count($paths) * 2;
+        foreach ($paths as $path_id) {
+            $post_pageviews = (int) ($pageviews_per_post * rand(5, 15) * 0.1);
+            $post_visitors = (int) (rand(1, 3) * $post_pageviews * 0.1);
+            array_push($values, $date, $path_id, 0, $post_pageviews, $post_visitors);
+
+        }
+
+        $placeholders = rtrim(str_repeat('(%s,%d,%d,%d,%d),', count($paths)), ',');
+        $wpdb->query($wpdb->prepare("INSERT INTO {$wpdb->prefix}koko_analytics_post_stats(date, path_id, post_id, pageviews, visitors) VALUES {$placeholders}", $values));
+    }
+
+    protected function seed_referrers(array $referrers, string $date, int $pageviews): void 
+    {
+        global $wpdb;
+        $values = array();
+        $pageviews_per_referrer = $pageviews / count($referrers) * 2;
+        foreach ($referrers as $referrer_id) {
+            $referrer_pageviews = (int) ($pageviews_per_referrer * rand(5, 15) * 0.1);
+            $referrer_visitors = (int) (rand(1, 3) * $referrer_pageviews * 0.1);
+            array_push($values, $date, $referrer_id, $referrer_pageviews, $referrer_visitors);
+        }
+        $placeholders = rtrim(str_repeat('(%s,%d,%d,%d),', count($referrers)), ',');
+        $wpdb->query($wpdb->prepare("INSERT INTO {$wpdb->prefix}koko_analytics_referrer_stats(date, id, pageviews, visitors) VALUES {$placeholders}", $values));
     }
 }
